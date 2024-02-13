c399ac71f09ef38cd65017cbed90ed3845636d2d
Ramesh Sukka <ramesh.sukka@hmcts.net>
2024-02-13 10:41:30 +0000

    Step-2: Enhanced checkout service with offers

--- src/main/scala/com/shopping/cart/service/CheckoutService.scala
+++ src/main/scala/com/shopping/cart/service/CheckoutService.scala
@@ -8,7 +8,14 @@ object CheckoutService {
 
   def checkout(basket: Basket, applyOffers: Boolean = false): BigDecimal = {
     if (applyOffers) {
-      ???
+      val appleCount = basket.count(_ == Apple)
+      val orangeCount = basket.count(_ == Orange)
+
+      val applesPrice = (appleCount/2 + appleCount%2) * Apple.price
+
+      val orangesPrice = ((2 * (orangeCount/3)) + orangeCount%3) * Orange.price
+
+      applesPrice + orangesPrice
     } else {
       basket.map(_.price).sum
     }
--- src/test/scala/com/shopping/cart/service/CheckoutServiceSpec.scala
+++ src/test/scala/com/shopping/cart/service/CheckoutServiceSpec.scala
@@ -15,11 +15,32 @@ class CheckoutServiceSpec extends AnyFlatSpec with Matchers {
     CheckoutService.checkout(basket) shouldBe BigDecimal(2.05)
   }
 
-  "CheckoutService" should "results 11.75 given checkbox contains 15 Apples and 11 Oranges" in {
+  "CheckoutService" should "results 11.75 given basket contains 15 Apples and 11 Oranges" in {
     CheckoutService.checkout(fillBasket(15, 11)) shouldBe BigDecimal(11.75)
   }
 
   private def fillBasket(apples: Int, oranges: Int): List[Fruit] = {
     List.fill(apples)(Apple) ++ List.fill(oranges)(Orange)
+  }
+
+  //CheckoutService with offers
+  "CheckoutService with offers" should "results 6.8 given basket contains 15 Apples and 11 Oranges" in {
+    CheckoutService.checkout(fillBasket(15, 11), true) shouldBe BigDecimal(4.8 + 2)
+  }
+
+  "CheckoutService with offers" should "results 2 given basket contains 0 Apples and 11 Oranges" in {
+    CheckoutService.checkout(fillBasket(0, 11), true) shouldBe BigDecimal(2)
+  }
+
+  "CheckoutService with offers" should "results 4.8 given basket contains 15 Apples and 0 Oranges" in {
+    CheckoutService.checkout(fillBasket(15, 0), true) shouldBe BigDecimal(4.8)
+  }
+
+  "CheckoutService with offers" should "results 1.1 given basket contains 2 Apples and 3 Oranges" in {
+    CheckoutService.checkout(fillBasket(2, 3), true) shouldBe BigDecimal(0.60 +0.50)
+  }
+
+  "CheckoutService with offers" should "results 1.1 given basket contains 1 Apples and 2 Oranges" in {
+    CheckoutService.checkout(fillBasket(1, 2), true) shouldBe BigDecimal(0.60 +0.50)
   }
 }
